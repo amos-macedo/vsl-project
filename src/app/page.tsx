0fb10179383b6291996d09d2cee9a8670f4f7655
amos-macedo <amosdemacedo007@gmail.com>
2025-09-05 22:59:56 -0300

    adding shad cn ui library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+"use client";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      Hello World
+      <Button onClick={() => alert("simboraaaa")}>clica aqui</Button>
+      {/* <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -97,7 +100,7 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
-      </footer>
+      </footer> */}
     </div>
   );
 }
