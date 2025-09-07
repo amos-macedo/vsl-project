32a37b753caf9a5ceef2eb9233ecd582a0aef5ba
amos-macedo <amosdemacedo007@gmail.com>
2025-09-07 17:35:43 -0300

    removed local video

--- src/components/home/video-player.tsx
+++ src/components/home/video-player.tsx
@@ -2,13 +2,12 @@
 
 export const VideoPlayer = () => {
   return (
-    <video
-      src="/video.mp4"
-      className="rounded-md"
-      controls
-      style={{ width: "100%", height: "auto" }}
-    >
-      Seu navegador não suporta vídeos.
+    <video controls width="100%" className="rounded-lg">
+      <source
+        src="https://www.dropbox.com/scl/fi/ke4d3d2atnc637eowt066/Emp-rio-da-sa-de-produtos-naturais..mp4?rlkey=5c8a85xt5c96o7tjleq3dj7pt&st=07h4ltlf&dl=1"
+        type="video/mp4"
+      />
+      Seu navegador não suporta a tag de vídeo.
     </video>
   );
 };
